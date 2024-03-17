e5a6631fa7d8952cc791e5a678b5d7b7815805ed
Divyanshu Pathak <divyanshupathak96@gmail.com>
2024-03-17 14:43:11 +0530

    Update app.js
    push it

--- src/app.js
+++ src/app.js
@@ -2,46 +2,35 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   if (req.method === 'POST') {
-    const chunks = [];
+    let body = '';
 
     req.on('data', chunk => {
-      const buf = Buffer.from(chunk);
-      const str = buf.toString();
-      chunks.push(str);
-      try{
-        const obj = JSON.parse(chunks.join(''));
-
-        const value1 = obj.num1;
-        const value2 = obj.num2;
-        if(value1<=0||value2<0)
-        {
-          res.writeHead(404,{"Content-Type":"plain/text"});
-          res.end("The operation cannot be performed");
-        }
-        else if(!isNaN(value1)&&!isNaN(value2)){
-          res.writeHead(200,{"Content-Type":"plain/text"});
-          res.end(`The result is ${value1**value2}`);
-        }
-        else{
-
-            res.writeHead(400,{"Content-Type":"plain/text"});
-            res.end("Bad Request");
-        }
-      }
-      catch(error){
-        res.writeHead(400,{"Content-Type":"plain/text"});
-        res.end("Bad Request");
-      
-      }
-
-
-
-
-      // Write code here to calculate power of a number
-      
+      body += chunk.toString(); 
     });
-    }
+
+    req.on('end', () => {
+      try {
+        const obj = JSON.parse(body); 
+        const value1 = parseInt(obj.num1, 10);
+        const value2 = parseInt(obj.num2, 10);
+
+        if (value1 <= 0 || value2 < 0) {
+          res.writeHead(404, { "Content-Type": "text/plain" });
+          res.end("The operation cannot be performed");
+        } else if (!isNaN(value1) && !isNaN(value2)) {
+          res.writeHead(200, { "Content-Type": "text/plain" });
+          const result = Math.pow(value1, value2);
+          res.end(`The result is ${result}`);
+        } else {
+          res.writeHead(400, { "Content-Type": "text/plain" });
+          res.end("Bad Request");
+        }
+      } catch (error) {
+        res.writeHead(400, { "Content-Type": "text/plain" });
+        res.end("Bad Request");
+      }
+    });
+  }
 });
 
 module.exports = server;
-      
