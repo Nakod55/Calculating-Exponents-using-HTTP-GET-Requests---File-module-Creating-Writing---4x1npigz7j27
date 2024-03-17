354ca934df1817ba3c3fcc4237ea6c2358e69dd3
Nakod55 <nakodvenkatesh43@gmail.com>
2024-03-17 14:28:16 +0530

    Sumbit

--- src/app.js
+++ src/app.js
@@ -11,19 +11,29 @@ const server = http.createServer((req, res) => {
       const obj = JSON.parse(chunks)
       const value1 = obj.num1;
       const value2 = obj.num2;
-      if(value1<=0||value2<0)
-      {
-        res.writeHead(404,{"Content-Type":"plain/text"});
-        res.end("The operation cannot be performed");
+      try{
+        if(value1<=0||value2<0)
+        {
+          res.writeHead(404,{"Content-Type":"plain/text"});
+          res.end("The operation cannot be performed");
+        }
+        else if(!isNaN(value1)&&!isNaN(value2)){
+          res.writeHead(200,{"Content-Type":"plain/text"});
+          res.end(`The result is ${value1**value2}`);
+        }
+        else{
+
+            res.writeHead(400,{"Content-Type":"plain/text"});
+            res.end("Bad Request");
+        }
       }
-      else if(!isNaN(value1)&&!isNaN(value2)){
-        res.writeHead(200,{"Content-Type":"plain/text"});
-        res.end(`The result is ${value1**value2}`);
-      }
-      else{
+      catch{
         res.writeHead(400,{"Content-Type":"plain/text"});
         res.end("Bad Request");
+      
       }
+
+
 
 
       // Write code here to calculate power of a number
